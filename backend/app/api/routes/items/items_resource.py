0d065b38371110fc318fed98cb71a53bc3bb0adb
apsareena <56432777+apsareena@users.noreply.github.com>
2023-04-11 19:34:40 +0000

    solved indentation error

--- backend/app/api/routes/items/items_resource.py
+++ backend/app/api/routes/items/items_resource.py
@@ -71,7 +71,7 @@ async def create_new_item(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=strings.ITEM_ALREADY_EXISTS,
         )
-     if not item_create.image:
+    if not item_create.image:
         response = openai.Image.create(
         prompt=item_create.title,
         n=1,
